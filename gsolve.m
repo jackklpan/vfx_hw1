d8c0f0a6d8e9ca5c138ad70f49492d77024d74c9
Jayis <jayis4176@gmail.com>
2014-03-14 18:53:48 +0800

    add comment

--- gsolve.m
+++ gsolve.m
@@ -1,3 +1,29 @@
+%
+% gsolve.m − Solve for imaging system response function
+%
+% Given a set of pixel values observed for several pixels in several
+% images with different exposure times, this function returns the
+% imaging system’s response function g as well as the log film irradiance
+% values for the observed pixels.
+%
+% Assumes:
+%
+% Zmin = 0
+% Zmax = 255
+%
+% Arguments:
+%
+% Z(i,j) is the pixel values of pixel location number i in image j
+% B(j) is the log delta t, or log shutter speed, for image j
+% l is lamdba, the constant that determines the amount of smoothness
+% w(z) is the weighting function value for pixel value z
+% 
+% Returns:
+%
+% g(z) is the log exposure corresponding to pixel value z
+% lE(i) is the log film irradiance at pixel location i
+%
+
 function [g,lE]=gsolve(Z,B,l,w)
   n = 256;
   A = zeros(size(Z,1)*size(Z,2)+n+1,n+size(Z,1));
