6c0b01c25e891069f36c4029a406b0967e76e62f
ej04xjp6 <ej04xjp6@gmail.com>
2014-03-14 19:43:47 +0800

    modify weightFunc

--- weightFunc.m
+++ weightFunc.m
@@ -1,4 +1,6 @@
-function [ weight ] = weightFunc( input, zmin, zmax )
+function [ weight ] = weightFunc( input )
+  zmin = 0;
+  zmax = 255;
   if input<=(1/2)*(zmin+zmax)
       weight = input-zmin;
   else
