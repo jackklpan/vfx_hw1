74b5927719ca7333ec8dcfe675e097b6cecf294f
Jayis <jayis4176@gmail.com>
2014-03-14 20:34:22 +0800

    1st & Failed

--- go.m
+++ go.m
@@ -2,10 +2,22 @@
 
 files = dir('./images/*.png');
 Z = [];
+denseX = 17; % vertical
+denseY = 13; % horizontal
+
 for i=1:length(files)
   pic = imread(['./images/',files(i).name]);
   picSize = size (pic);
-  R = reshape (pic(:,:,1), picSize(1) * picSize(2), 1);
+  
+  row = int16(picSize (1) / (denseX + 1));
+  col = int16(picSize (2) / (denseY + 1));
+  
+  R = [];
+  for x = 1: denseX
+      for y = 1: denseY
+          R = [R; pic(x * row, y * col, 1)];
+      end
+  end
   Z = [Z R];
 end
 
@@ -14,7 +26,10 @@ end
 fileID = fopen ('./images/exposures1.txt');
 expo = textscan (fileID, '%f');
 expo = expo{1,1};
-shutterSpeed = log (1 ./ expo);
+shutterSpeed = reshape (log (1 ./ expo), 1, length(expo));
+for i = 2: denseX * denseY
+    shutterSpeed(i, :) = shutterSpeed(1, :);
+end
 fclose(fileID);
 
 %%
--- gsolve.m
+++ gsolve.m
@@ -1,9 +1,9 @@
 %
-% gsolve.m − Solve for imaging system response function
+% gsolve.m ??Solve for imaging system response function
 %
 % Given a set of pixel values observed for several pixels in several
 % images with different exposure times, this function returns the
-% imaging system’s response function g as well as the log film irradiance
+% imaging system?�s response function g as well as the log film irradiance
 % values for the observed pixels.
 %
 % Assumes:
@@ -23,26 +23,28 @@
 % g(z) is the log exposure corresponding to pixel value z
 % lE(i) is the log film irradiance at pixel location i
 %
-
 function [g,lE]=gsolve(Z,B,l,w)
-  n = 256;
-  A = zeros(size(Z,1)*size(Z,2)+n+1,n+size(Z,1));
-  b = zeros(size(A,1),1);
-  k = 1; %% Include the data-fitting equations for i=1:size(Z,1)
-  for j=1:size(Z,2)
-    wij = w(Z(i,j)+1);
-    A(k,Z(i,j)+1) = wij; A(k,n+i) = -wij; b(k,1) = wij * B(i,j);
-    k=k+1;
-  end
-  A(k,129) = 1;
-  k=k+1;
-  %% Fix the curve by setting its middle value to 0
-  %% Include the smoothness equations
-  for i=1:n-2
-    A(k,i)=l*w(i+1); A(k,i+1)=-2*l*w(i+1); A(k,i+2)=l*w(i+1);
-    k=k+1;
-  end
-  x = A\b; %% Solve the system using SVD
-  g = x(1:n);
-  lE = x(n+1:size(x,1));
+n = 256;
+A = zeros(size(Z,1)*size(Z,2)+n+1,n+size(Z,1));
+b = zeros(size(A,1),1);
+%% Include the data-fitting equations
+k = 1;
+for i=1:size(Z,1)
+ for j=1:size(Z,2)
+ wij = w(Z(i,j)+1);
+ A(k,Z(i,j)+1) = wij; A(k,n+i) = -wij; b(k,1) = wij * B(i,j);
+ k=k+1;
+ end
 end
+%% Fix the curve by setting its middle value to 0
+A(k,129) = 1;
+k=k+1;
+%% Include the smoothness equations
+for i=1:n-2
+ A(k,i)=l*w(i+1); A(k,i+1)=-2*l*w(i+1); A(k,i+2)=l*w(i+1);
+ k=k+1;
+end
+%% Solve the system using SVD
+x = A\b;
+g = x(1:n);
+lE = x(n+1:size(x,1));
