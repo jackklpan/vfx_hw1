29232bef9d0eb154f1c0bd3cb20ad99cd4ce6d9d
Jayis <jayis4176@gmail.com>
2014-03-16 21:07:32 +0800

    tonemap Global fail

---
+++ tonemap_photo.m
@@ -0,0 +1,19 @@
+function TMpic = tonemap_photo( HDRpic )
+
+a = 0.5;
+H = rgb2ycbcr (HDRpic);
+L = H (:, :, 1);
+sizeL = size (L);
+
+%% Global Operator
+L_w = exp (sum (sum (log (0.001 + L))) / (sizeL(1) * sizeL(2)));
+L_m = (a / L_w) * L;
+L_d = L_m ./ (1 + L_m);
+
+
+%% output
+H (:, :, 1) = L_d;
+TMpic = ycbcr2rgb (H);
+
+
+end
