72af05037830240421b31ef25236f140c9b4ca8a
jackklpan <jackklpan@gmail.com>
2014-03-25 14:08:34 +0800

    change alignment to adaptive thresholding

--- alignment.m
+++ alignment.m
@@ -13,17 +13,12 @@ function [shift_ref_output] = alignment( img1, img2, shift_bits, tolerance )
   end
   
   img_size = size(img1);
-  img1_median = median(reshape(img1, 1, img_size(1)*img_size(2)));
-  img2_median = median(reshape(img2, 1, img_size(1)*img_size(2)));
+  
+  [upper_bound1, lower_bound1, img1_median, upper_bound2, lower_bound2, img2_median] = getGoodThreshloding(img1, img2, tolerance);
+  img1_exclusion = (img1>=upper_bound1) | (img1<=lower_bound1);
   img1_thresholding = im2bw(img1, double(img1_median)/255);
+  img2_exclusion = (img2>=upper_bound2) | (img2<=lower_bound2);
   img2_thresholding = im2bw(img2, double(img2_median)/255);
- 
-  upper_bound = img1_median+tolerance;
-  lower_bound = img1_median-tolerance;
-  img1_exclusion = (img1>=upper_bound) | (img1<=lower_bound);
-  upper_bound = img2_median+tolerance;
-  lower_bound = img2_median-tolerance;
-  img2_exclusion = (img2>=upper_bound) | (img2<=lower_bound);
   
   min_err = img_size(1)*img_size(2);
   for i=-1:1
@@ -44,6 +39,44 @@ function [shift_ref_output] = alignment( img1, img2, shift_bits, tolerance )
           end
       end
   end
+end
+
+function [upper_bound1, lower_bound1, thresholding1, upper_bound2, lower_bound2, thresholding2] = getGoodThreshloding(img1, img2, tolerance)
+  thresholding1 = prctile(img1(:),50.5);
+  thresholding2 = prctile(img2(:),50.5);
+
+  percent = 50.5;
+  upper_bound1 = prctile(img1(:),percent)+tolerance;
+  upper_bound2 = prctile(img2(:),percent)+tolerance;
+  while(upper_bound2 >= 255 && percent > 1)
+     percent = percent/2;
+     upper_bound1 = prctile(img1(:),percent)+tolerance;
+     upper_bound2 = prctile(img2(:),percent)+tolerance;
+  end
+  if(percent ~= 50)
+      thresholding1 = prctile(img1(:),percent);
+      thresholding2 = prctile(img2(:),percent);
+      lower_bound1 = prctile(img1(:),percent)-tolerance;
+      lower_bound2 = prctile(img2(:),percent)-tolerance;
+      return;
+  end
+  
+  percent = 50.5;
+  lower_bound1 = prctile(img1(:),percent)-tolerance;
+  lower_bound2 = prctile(img2(:),percent)-tolerance;
+  while(lower_bound2 <= 0 && percent < 99)
+     percent = percent*2;
+     lower_bound1 = prctile(img1(:),percent)-tolerance;
+     lower_bound2 = prctile(img2(:),percent)-tolerance;
+  end
+  if(percent ~= 50)
+      thresholding1 = prctile(img1(:),percent);
+      thresholding2 = prctile(img2(:),percent);
+      upper_bound1 = prctile(img1(:),percent)+tolerance;
+      upper_bound2 = prctile(img2(:),percent)+tolerance;
+      return;
+  end
+  
 end
 
 % files = dir('./images/*.png');
